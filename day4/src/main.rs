b51f651fb4e47d534ec9ef761688ed8333654e2b
dwardy <61111939+dillon-ward@users.noreply.github.com>
2024-12-04 21:00:06 +0000

    day4cleanup

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -8,70 +8,77 @@ fn main() {
     println!("Part 2: {}", part2(&input));
 }
 
-fn get_xmas(l: &List, i: usize, p:usize) -> u64 {
-    let mut finds = 0;
-
-    if i >= 3 && l[i-1].chars().nth(p) == Some('M') && l[i-2].chars().nth(p) == Some('A') && l[i-3].chars().nth(p) == Some('S') {
-        finds += 1;
-    }
-
-    if i >= 3 && p <= l[i].len() - 4 && l[i-1].chars().nth(p+1) == Some('M') && l[i-2].chars().nth(p+2) == Some('A') && l[i-3].chars().nth(p+3) == Some('S') {
-        finds += 1;
-    }
-
-    if p <= l[i].len() - 4 && l[i][p+1..p+4] == *"MAS" {
-        finds += 1;
-    }
-
-    if i <= l.len() - 4 && p <= l[i].len() - 4 && l[i+1].chars().nth(p+1) == Some('M') && l[i+2].chars().nth(p+2) == Some('A') && l[i+3].chars().nth(p+3) == Some('S') {
-        finds += 1;
-    }
-
-    if i <= l.len() - 4 && l[i+1].chars().nth(p) == Some('M') && l[i+2].chars().nth(p) == Some('A') && l[i+3].chars().nth(p) == Some('S') {
-        finds += 1;
-    }
-
-    if i <= l.len() - 4 && p >= 3 && l[i+1].chars().nth(p-1) == Some('M') && l[i+2].chars().nth(p-2) == Some('A') && l[i+3].chars().nth(p-3) == Some('S') {
-        finds += 1;
-    }
-
-    if p >= 3 && l[i][p-3..p] == *"SAM" {
-        finds += 1;
-    }
-
-    if i >= 3 && p >= 3 && l[i-1].chars().nth(p-1) == Some('M') && l[i-2].chars().nth(p-2) == Some('A') && l[i-3].chars().nth(p-3) == Some('S') {
-        finds += 1;
-    }
-
-    finds
-}
-
-fn get_x_mas(l: &List, i: usize, p:usize) -> u64 {
-    if i >= 1 && i <= l.len() - 2 && p >= 1 && p <= l.len() - 2 &&
-                ((l[i-1].chars().nth(p-1) == Some('M') && l[i+1].chars().nth(p+1) == Some('S')) || (l[i-1].chars().nth(p-1) == Some('S') && l[i+1].chars().nth(p+1) == Some('M'))) &&
-                ((l[i-1].chars().nth(p+1) == Some('M') && l[i+1].chars().nth(p-1) == Some('S')) || (l[i-1].chars().nth(p+1) == Some('S') && l[i+1].chars().nth(p-1) == Some('M'))) {return 1}
-    0
-}
-
-fn part1(input: &List) -> u64 {
+fn part1(l: &List) -> u64 {
     let mut result = 0;
-    for i in 0..input.len() {
+    for i in 0..l.len() {
         let mut pos = 0;
-        while let Some(p) = input[i][pos..].find('X') {
-            result += get_xmas(input, i, pos + p);
-            pos += p + 1;
+        while let Some(p) = l[i][pos..].find('X') {
+            pos += p;
+            
+            // N
+            if  i >= 3 && 
+                l[i-1].chars().nth(pos) == Some('M') && 
+                l[i-2].chars().nth(pos) == Some('A') && 
+                l[i-3].chars().nth(pos) == Some('S') {result += 1}
+
+            // NE
+            if  i >= 3 && pos <= l[i].len() - 4 && 
+                l[i-1].chars().nth(pos+1) == Some('M') && 
+                l[i-2].chars().nth(pos+2) == Some('A') && 
+                l[i-3].chars().nth(pos+3) == Some('S') {result += 1}
+
+            // E
+            if  pos <= l[i].len() - 4 && 
+                l[i][pos+1..pos+4] == *"MAS" {result += 1}
+
+            // SE
+            if  i <= l.len() - 4 && pos <= l[i].len() - 4 &&
+                l[i+1].chars().nth(pos+1) == Some('M') && 
+                l[i+2].chars().nth(pos+2) == Some('A') && 
+                l[i+3].chars().nth(pos+3) == Some('S') {result += 1}
+
+            // S
+            if  i <= l.len() - 4 && 
+                l[i+1].chars().nth(pos) == Some('M') && 
+                l[i+2].chars().nth(pos) == Some('A') && 
+                l[i+3].chars().nth(pos) == Some('S') {result += 1}
+
+            // SW
+            if  i <= l.len() - 4 && pos >= 3 && 
+                l[i+1].chars().nth(pos-1) == Some('M') && 
+                l[i+2].chars().nth(pos-2) == Some('A') && 
+                l[i+3].chars().nth(pos-3) == Some('S') {result += 1}
+
+            // W
+            if  p >= 3 && 
+                l[i][pos-3..pos] == *"SAM" {result += 1;}
+
+            // NW
+            if  i >= 3 && pos >= 3 && 
+                l[i-1].chars().nth(pos-1) == Some('M') && 
+                l[i-2].chars().nth(pos-2) == Some('A') && 
+                l[i-3].chars().nth(pos-3) == Some('S') {result += 1}
+
+            pos += 1;
         }
     }
     result
 }
 
-fn part2(input: &List) -> u64 {
+fn part2(l: &List) -> u64 {
     let mut result = 0;
-    for i in 0..input.len() {
-        let mut pos = 0;
-        while let Some(p) = input[i][pos..].find('A') {
-            result += get_x_mas(input, i, pos + p);
-            pos += p + 1;
+    for i in 1..l.len()-1 {
+        let mut pos = 1;
+        while let Some(p) = l[i][pos..].find('A') {
+            pos += p;
+
+            if  pos <= l.len() - 2 &&
+                (   (l[i-1].chars().nth(pos-1) == Some('M') && l[i+1].chars().nth(pos+1) == Some('S')) || 
+                    (l[i-1].chars().nth(pos-1) == Some('S') && l[i+1].chars().nth(pos+1) == Some('M'))  ) &&
+                (   (l[i-1].chars().nth(pos+1) == Some('M') && l[i+1].chars().nth(pos-1) == Some('S')) || 
+                    (l[i-1].chars().nth(pos+1) == Some('S') && l[i+1].chars().nth(pos-1) == Some('M'))  ) {result += 1}
+
+            pos += 1;
         }
     }
     result
@@ -81,7 +88,7 @@ fn get_input(filename: &str) -> List {
     fs::read_to_string(filename).expect("Should have read file")
         .lines()
         .map(|l| {
-            l.split("")
+            l.chars()
             .collect()
         })
         .collect()
